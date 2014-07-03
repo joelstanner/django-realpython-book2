95f25631928e0e07382ef43abfc13536c7b0b3b3
poolbath1 <eyftickid@poolbath1.33mail.com>
2014-07-03 04:24:14 -0700

    next parts through pg 264 nav collapse button not working

--- bloggy/blog/urls.py
+++ bloggy/blog/urls.py
@@ -3,4 +3,5 @@ from blog import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
-                       )
+                       url(r'^(?P<post_name>\w+)/$', views.post, name='post'),
+                       )               
--- bloggy/bloggy/urls.py
+++ bloggy/bloggy/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from settings import MEDIA_ROOT
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,4 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': MEDIA_ROOT}),
 )
